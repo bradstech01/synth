5391431835a488f4e1753f61c31b703c10d448b7
bstech <ebstech1993@gmail.com>
2022-04-15 09:47:55 -0400

    midi, 64-step sequencer

--- src/components/keyboard/index.jsx
+++ src/components/keyboard/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PianoKey } from '../pianoKey';
-import keyMap from '../../scripts/keyMap.js';
 
 /**
  * Component containing the visual keyboard; the "money maker", as it were.
@@ -10,90 +9,33 @@ import keyMap from '../../scripts/keyMap.js';
 export class Keyboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currentlyPlaying: [],
-      isMouseDown: false,
-      isKeyDown: false,
-    };
   }
 
   static propTypes = {
     role: PropTypes.string,
+    currentlyPlaying: PropTypes.array.isRequired,
+    isMouseDown: PropTypes.bool.isRequired,
+    isKeyDown: PropTypes.bool.isRequired,
     triggerNote: PropTypes.func.isRequired,
     triggerRelease: PropTypes.func.isRequired,
-  };
-
-  componentDidMount() {
-    document.addEventListener('mousedown', this.setMouseFlag);
-    document.addEventListener('mouseup', this.setMouseFlag);
-    document.addEventListener('keydown', this.handleKeyPress);
-    document.addEventListener('keyup', this.handleKeyRelease);
-  }
-
-  handleKeyPress = (e) => {
-    if (!this.state.isKeyDown) this.setState({ isKeyDown: true });
-
-    if (keyMap(e.key)) this.addToCurrentlyPlaying(keyMap(e.key));
-  };
-
-  //handles the key release within app; does not get passed around
-  handleKeyRelease = (e) => {
-    if (keyMap(e.key)) this.removeFromCurrentlyPlaying(keyMap(e.key));
-
-    if (this.state.currentlyPlaying.length === 0)
-      this.setState({ isKeyDown: false });
-  };
-
-  addToCurrentlyPlaying = (note) => {
-    if (!this.state.currentlyPlaying.includes(note)) {
-      let newPlaying = [...this.state.currentlyPlaying];
-      newPlaying.push(note);
-      this.setState({
-        currentlyPlaying: newPlaying,
-      });
-    }
-  };
-
-  removeFromCurrentlyPlaying = (note) => {
-    if (this.state.currentlyPlaying.includes(note)) {
-      let currentlyPlaying = [...this.state.currentlyPlaying];
-      let newPlaying = currentlyPlaying.filter((value) => {
-        return value !== note;
-      });
-      this.setState({
-        currentlyPlaying: newPlaying,
-      });
-    }
-  };
-
-  setMouseFlag = (e) => {
-    e.stopPropagation();
-    if (e.type === 'mousedown') this.setState({ isMouseDown: true });
-    else this.setState({ isMouseDown: false });
-
-    if (!this.state.isKeyDown && this.state.isMouseDown) {
-      document.removeEventListener('keydown', this.handleKeyPress);
-      document.removeEventListener('keyup', this.handleKeyRelease);
-    }
-    if (!this.state.isMouseDown) {
-      document.addEventListener('keydown', this.handleKeyPress);
-      document.addEventListener('keyup', this.handleKeyRelease);
-    }
+    setMouseFlag: PropTypes.func.isRequired,
+    onMouseDown: PropTypes.func.isRequired,
+    onMouseUp: PropTypes.func.isRequired,
   };
 
   renderPianoKey(note, octave, octaveShift, triggerKey, hiddenOnMobile) {
     return (
       <PianoKey
         note={note + (octave + octaveShift)}
-        isKeyDown={this.state.isKeyDown}
-        currentlyPlaying={this.state.currentlyPlaying.includes(
+        isKeyDown={this.props.isKeyDown}
+        currentlyPlaying={this.props.currentlyPlaying.includes(
           note + (octave + octaveShift)
         )}
         triggerKey={triggerKey}
         triggerNote={this.props.triggerNote}
         triggerRelease={this.props.triggerRelease}
-        onMouseDown={this.addToCurrentlyPlaying}
-        onMouseUp={this.removeFromCurrentlyPlaying}
+        onMouseDown={this.props.onMouseDown}
+        onMouseUp={this.props.onMouseUp}
         hiddenOnMobile={hiddenOnMobile}
       />
     );
@@ -104,8 +46,8 @@ export class Keyboard extends React.Component {
       <div
         className="keyboard"
         role="button"
-        onMouseDown={this.setMouseFlag}
-        onMouseUp={this.setMouseFlag}
+        onMouseDown={this.props.setMouseFlag}
+        onMouseUp={this.props.setMouseFlag}
       >
         {this.renderPianoKey('C', 3, 0, 'q')}
         {this.renderPianoKey('C#', 3, 0, '2')}
